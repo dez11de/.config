cf41a197c338616cd724ee00933d72565170cea5
dez11de <dennis@ganzeinfach.nl>
2022-04-27 12:56:45 +0200

    Fixes italic font, finally
    NeoVims :checkhealth complains, but everything seems to be working.

--- nvim/lua/plugins/nightfox.lua
+++ nvim/lua/plugins/nightfox.lua
@@ -6,9 +6,9 @@ require('nightfox').setup({
     compile_file_suffix = "_compiled", -- Compiled file suffix
     transparent = false,    -- Disable setting background
     terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-    dim_inactive = false,   -- Non focused panes set to alternative background
+    dim_inactive = true,   -- Non focused panes set to alternative background
     styles = {              -- Style to be applied to different syntax groups
-      comments = "NONE",    -- Value is any valid attr-list value `:help attr-list`
+      comments = "italic",    -- Value is any valid attr-list value `:help attr-list`
       functions = "NONE",
       keywords = "NONE",
       numbers = "NONE",
